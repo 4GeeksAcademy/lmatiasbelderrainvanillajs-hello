4c41db452cc9fe4d4ccec445a3b161b939361f71
lmatiasbelderrain <Matibelde@hotmail.com>
2025-02-11 20:42:47 +0000

    comienzo chequep

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("error-msg").style.display = "none"
+  document.getElementById("sendForm").addEventListener("click", chequeo)
 };
+
+function chequeo() {
+  let tarjeta = document.getElementById("card").value
+  if (tarjeta == "") {
+    alert("missing card number");
+    document.getElementById("error-msg").style.display = "block"
+    return 
+  }
+  let cvcnumber = document.getElementById("cvc").value
+  if (cvcnumber == "") {
+    alert("missing cvc number")
+    document.getElementById("error-msg").style.display = "block"
+    return
+  }
+}
